7df53844ea5d58a65f3c166b8f0056fd5a8ec401
Matias Tomas <tomasrimolimatias@gmail.com>
2019-10-11 17:18:09 -0300

    addddd

--- src/OrgAgropecuaria.java
+++ src/OrgAgropecuaria.java
@@ -3,36 +3,55 @@ import java.util.ArrayList;
 
 public class OrgAgropecuaria extends ElementoAgropecuario {
 	private String tipo;
-	private ArrayList<AnimalAgro> animales;
+	private ArrayList<ElementoAgropecuario> elementos;
 	
 	public OrgAgropecuaria (String tip){
 		tipo = tip;
-		animales = new ArrayList<AnimalAgro>();
+		elementos = new ArrayList<ElementoAgropecuario>();
+	}
+	
+	public boolean esAnimal(){
+		return false;
 	}
 	
 	public void setTipo (String tip){
 		tipo = tip;
 	}
+	
 	public String getTipo(){
 		return tipo;
 	}
+	
 	public ArrayList<AnimalAgro> getAnimales(){
+		ArrayList<AnimalAgro> animales = new ArrayList<AnimalAgro>() ;
+		for (ElementoAgropecuario elem : elementos){
+			if (elem.esAnimal())
+				animales.add( (AnimalAgro) elem);
+			else
+				((OrgAgropecuaria)elem).getAnimales();
+		}
 		return animales;
 	}
-	public void addAnimales(AnimalAgro animal){
-		animales.add(animal);
+	public void addElementos(ElementoAgropecuario elemento){
+		elementos.add(elemento);
 	}
 	
 	public ArrayList<AnimalAgro> buscar(Criterio c){
 		ArrayList<AnimalAgro> res = new ArrayList<AnimalAgro>();
-		for (AnimalAgro a : animales){
-			if (c.cumple(a)){
-				res.add(a);
+		for (ElementoAgropecuario elem : elementos){
+			if ((elem.esAnimal()) && (c.cumple((AnimalAgro)elem)))
+				res.add((AnimalAgro) elem);
+			else{
+				res.addAll(((OrgAgropecuaria)elem).buscar(c));
+				
 			}
 		}	
 		return res;	
 	}
 	
+	public int cantidadAnimales (ArrayList<AnimalAgro> animales){
+		return 0;
+	}
 	
 	
 	
