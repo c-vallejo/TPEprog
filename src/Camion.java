53ee9e02fe08848c0ed7b548d4b36adad27ac52f
c-vallejo <charlytandil@gmail.com>
2019-10-26 17:03:55 -0300

    change in camion

--- src/Camion.java
+++ src/Camion.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 
 public class Camion {
 	
 	private int capacidad;
 	private Criterio crit;
+	private ArrayList<AnimalAgro> carga;
 	
 	public Camion(int cap, Criterio c) {
 		capacidad = cap;
 		crit = c;
+		carga = new ArrayList<AnimalAgro>();
 	}
 
 	/**
@@ -22,4 +25,9 @@ public class Camion {
 	public Criterio getCriterio() {
 		return crit;
 	}
+	
+	public void cargarCamion(AnimalAgro a) {
+		carga.add(a);
+	}
+	
 }
