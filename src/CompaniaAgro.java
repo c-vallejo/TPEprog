cf989ba17aab16bbd576660692385a930e0f02b8
c-vallejo <charlytandil@gmail.com>
2019-11-02 17:45:19 -0300

    added Compania

---
+++ src/CompaniaAgro.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class CompaniaAgro {
+	
+	private String nombre;
+	private OrgAgropecuaria principal;
+	private ArrayList<Clasificador> clasificacion;
+	
+	public CompaniaAgro(String n, String tipo) {
+		nombre = n;
+		principal = new OrgAgropecuaria(tipo);
+		clasificacion = new ArrayList<Clasificador>();
+	}
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public void addOrg(OrgAgropecuaria org) {
+		principal.addElementos(org);
+	}
+	
+	public void addClasificador(Clasificador cla) {
+		clasificacion.add(cla);
+	}
+	
+	public String clasificar(AnimalAgro ani){
+	
+		Integer a = ani.getId();
+		String categoria = a.toString() ;
+		
+		for (Clasificador cla : clasificacion) {
+			String aux =cla.clasificar(ani);
+			if (aux!=null)
+				categoria += "-" + aux;
+		}
+		
+		return categoria;
+	}
+	
+	public void vender(Camion c, OrgAgropecuaria org) {
+		  c.cargarCamion(org);
+		}
+}
